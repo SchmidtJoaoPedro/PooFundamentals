92e0112d0801bba62b5ea58121db335abd43ba10
SchmidtJoaoPedro <joaoschmidtmagalhaes@gmail.com>
2026-02-28 14:58:57 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_01
+{
+    internal class Program
+    {
+        public static double Calculo(double B, double E)
+        {
+            if (E == 1)
+            {
+                return B;
+            }
+            else {
+                return B * Calculo(B, E - 1);
+            }
+        }
+        public static double IMC(double H, double P)
+        {
+            return P / Calculo (H, 2);
+        }
+        
+        static void Main(string[] args)
+        {
+            string name;
+            double peso,altura;
+            double imc;
+
+            Console.WriteLine("Escreva seu nome: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"{name} Entre com sua altura:");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{name} Entre com sua peso:");
+            peso = double.Parse(Console.ReadLine());
+
+            imc = IMC(altura, peso);
+            Console.WriteLine($"{name} seu Indice de masa corporal: {imc}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0, op2 = 0;
+            string nomeProduto = "";
+            double valorProduto = 0;
+            string formaPagamento = "";
+            do
+            {
+                Console.WriteLine("1.None do Produto");
+                Console.WriteLine("2.Valor do Produto");
+                Console.WriteLine("3.Forma do Pagamento");
+                Console.WriteLine("4.Mostrar Dados da Compra");
+                Console.Write("5.Sair");
+                Console.WriteLine("Escolha sua opcão: ");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("1.None do Produto");
+                        nomeProduto = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("2.Valor do Produto");
+                        valorProduto = double.Parse(Console.ReadLine());
+
+                        break;
+                    case 3:
+                        Console.WriteLine("3.Forma do Pagamento");
+                        do
+                        {
+                            Console.WriteLine("1.Dinheiro");
+                            Console.WriteLine("3.Pix");
+                            Console.WriteLine("Cartão");
+
+                            Console.WriteLine("Escolha sua opção:");
+                            op2 = int.Parse(Console.ReadLine());
+                            switch (op2)
+                            {
+                                case 1:
+                                    formaPagamento = "Dinheiro";
+                                    break;
+                                case 2:
+                                    formaPagamento = "Pix";
+                                    break;
+
+                                case 3:
+                                    formaPagamento = "Cartão";    
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Saindo...");
+                                    break;
+                            }
+                        } while (op2 != 4);
+                        break;
+
+                    case 4:
+                        Console.WriteLine($"Produto: {nomeProduto}\n Valor: {valorProduto}\n Forma de Pagamento");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+
+                        break;
+                }
+            } while (op != 5);
+        }
+
+    }
+}
